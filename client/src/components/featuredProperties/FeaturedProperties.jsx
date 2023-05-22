09da4c7ae5d8fbba0b061e6e5574f0e818b9cff0
Ambuj Singh <7ambujsingh@gmail.com>
2023-05-22 12:59:20 +0530

    #update

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -16,7 +16,7 @@ const FeaturedProperties = () => {
 							<span className="fpName">{item.name}</span>
 							<span className="fpCity">{item.city}</span>
 							<span className="fpPrice">
-								Starting from ${item.cheapestPrice}
+								Starting from ₹{item.cheapestPrice}
 							</span>
 							{item.rating && (
 								<div className="fpRating">
