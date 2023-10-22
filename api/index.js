23d827abb1243638370e8ddeeb231e1380401efb
Ambuj Singh <7ambujsingh@gmail.com>
2023-10-23 00:20:04 +0530

    #update

--- api/index.js
+++ api/index.js
@@ -12,24 +12,22 @@ const app = express();
 dotenv.config();
 
 const connect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO);
-    console.log("Connected to mongoDB.");
-  } catch (error) {
-    throw error;
-  }
+	try {
+		await mongoose.connect(process.env.MONGO);
+		console.log("Connected to mongoDB.");
+	} catch (error) {
+		throw error;
+	}
 };
 
 mongoose.connection.on("disconnected", () => {
-  console.log("mongoDB disconnected!");
+	console.log("mongoDB disconnected!");
 });
 
 //middlewares
-app.use(cors())
-app.use(cookieParser())
+app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
-
-//! test started
 
 app.get("/", (req, res) => {
 	res.send("Ok Tested!");
@@ -43,17 +41,17 @@ app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 
 app.use((err, req, res, next) => {
-  const errorStatus = err.status || 500;
-  const errorMessage = err.message || "Something went wrong!";
-  return res.status(errorStatus).json({
-    success: false,
-    status: errorStatus,
-    message: errorMessage,
-    stack: err.stack,
-  });
+	const errorStatus = err.status || 500;
+	const errorMessage = err.message || "Something went wrong!";
+	return res.status(errorStatus).json({
+		success: false,
+		status: errorStatus,
+		message: errorMessage,
+		stack: err.stack,
+	});
 });
 
 app.listen(8800, () => {
-  connect();
-  console.log("Connected to backend.");
+	connect();
+	console.log("Connected to backend.");
 });
